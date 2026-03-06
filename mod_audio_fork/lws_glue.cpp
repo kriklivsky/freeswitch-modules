238d55b08c96f10a5946e4f54fb814269b260df3
kriklivsky <mail@kriklivsky.com>
2026-03-06 18:18:35 +0300

    Fix null pointer dereference in fork_session_cleanup

--- mod_audio_fork/lws_glue.cpp
+++ mod_audio_fork/lws_glue.cpp
@@ -739,11 +739,10 @@ extern "C" {
       return SWITCH_STATUS_FALSE;
     }
     private_t* tech_pvt = (private_t*) switch_core_media_bug_get_user_data(bug);
+    if (!tech_pvt) return SWITCH_STATUS_FALSE;
     uint32_t id = tech_pvt->id;
 
     switch_log_printf(SWITCH_CHANNEL_SESSION_LOG(session), SWITCH_LOG_DEBUG, "(%u) fork_session_cleanup\n", id);
-
-    if (!tech_pvt) return SWITCH_STATUS_FALSE;
     drachtio::AudioPipe *pAudioPipe = static_cast<drachtio::AudioPipe *>(tech_pvt->pAudioPipe);
       
     switch_mutex_lock(tech_pvt->mutex);
